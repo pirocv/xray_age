201fb0104cb16a968e8d9e4aa4e2e207eaa499ae
ieki <ieki.tky@gmail.com>
2022-01-31 20:56:37 +0900

    add predict.py README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# xray_age
+- Predict biological age from frontal CXR image.
+
+
+## supported file type
+- jpeg
+- png
+
+## usage
+- Download pre-trained model
+  - https://riken-share.box.com/s/0zddg8a5242cffgpr84x2y5lnoqo93h7
+
+```python
+python predict.py path_to_cxr_image
+```
+## requirements
+- Python3 (3.8.5)
+- Pytorch (1.4.0)
+- Torchvision (0.5.0)
+- Fastai (1.0.61)
+
---
+++ predict.py
@@ -0,0 +1,22 @@
+from fastai.vision import *
+import argparse
+
+def main():
+    parser=argparse.ArgumentParser(description='predict X-ray age from frontal CXR.')
+    parser.add_argument('image', help='CXR image file path')
+    args=parser.parse_args()
+    image=args.image
+
+    learn=load_learner(
+        '../tmp/','every_model_age_senet154_v2_tl_26_ft_7_fp32.pkl'
+    )
+    out=learn.predict(open_image(image))[0]
+
+    print(f"CXR age: {round(out.data[0])}")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
